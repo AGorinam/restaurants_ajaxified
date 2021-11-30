4fd9f5bede6bc7fc1e7a32b98122314869b36e1b
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-11-30 11:41:54 +0100

    Added Reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
     resources :restaurants, only: [ :index, :show ] do
       resources :reviews, only: :create
     end
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
